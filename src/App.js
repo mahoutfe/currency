7ef5ad74384efff1fe3063bd5df7aa231677f12d
mahout <93513481+muhahoi@users.noreply.github.com>
2024-06-21 17:05:15 +0300

    исправил ошибки

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+const getResource = async (currency) => {
+	let res = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+
+	if (!res.ok) {
+		throw new Error(`HTTP error!, status: ${res.status}`);
+	}
+
+	const data = await res.json();
+
+	return data;
+};
+
 const App = (props) => {
 	const [course, setCourse] = useState(props.course);
 	const [currency, setCurrency] = useState('RUB');
 
-	const getResource = async (currency) => {
-		if (currency != 'RUB') {
-			let res = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
-
-			if (!res.ok) {
-				throw new Error(`HTTP error!, status: ${res.status}`);
-			}
-
-			const data = await res.json();
-
-			console.log(data.Valute[currency].Value);
-			const sum = Math.round(course * data.Valute[currency].Value * 100) / 100;
-			setCourse((course) => `${sum} ${currency}`);
-		}
-	};
-
-	function getCurrCode(newCode) {
+	function setCurrencyCode(newCode) {
 		setCourse(props.course);
 		setCurrency(newCode);
 	}
 
+	function toogleCourse(data, currency) {
+		setCourse(
+			(course) =>
+				`${
+					Math.round(course * data.Valute[currency].Value * 100) / 100
+				} ${currency}`
+		);
+	}
+
 	useEffect(() => {
-		getResource(currency);
+		if (currency !== 'RUB') {
+			getResource()
+				.then((res) => toogleCourse(res, currency))
+				.catch((error) => console.error('Ошибка при получении данных:', error));
+		}
 	}, [currency]);
 
 	function resetCourse() {
 		setCourse(props.course);
 	}
 
-	const Initial = () => {
-		return <div className='initial'>{props.course} рублей равно:</div>;
-	};
-
 	return (
 		<div className='app'>
 			{course !== props.course ? (
-				<Initial />
+				<div className='initial'>{props.course} рублей равно:</div>
 			) : (
 				<div className='initial'>Введите сумму</div>
 			)}
 			<div className='course'>{course}</div>
 			<div className='controls'>
-				<button onClick={() => getCurrCode('USD')}>USD</button>
-				<button onClick={() => getCurrCode('EUR')}>EUR</button>
-				<button onClick={() => getCurrCode('GBP')}>GBP</button>
+				<button onClick={() => setCurrencyCode('USD')}>USD</button>
+				<button onClick={() => setCurrencyCode('EUR')}>EUR</button>
+				<button onClick={() => setCurrencyCode('GBP')}>GBP</button>
 				<button onClick={() => resetCourse()}>RESET</button>
 			</div>
 		</div>
